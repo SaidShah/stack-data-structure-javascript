e732866bd407b1ad857befa57a965fee2df0d38e
said shah <saidmysr20@aol.com>
2019-01-24 14:07:11 -0500

    created files

---
+++ index.js
@@ -0,0 +1,10 @@
+// Stacks
+
+//push => to push an element onto a Stacks
+
+// pop => to remove the top element of a stack
+
+// peek => to display the top element of a stack
+
+// length => to view the length of a stack
+let stack = [] // this is the stack
